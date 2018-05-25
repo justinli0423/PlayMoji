8807f48dd6e803ab8e79f968c2cb8e68e740614d
Justin Li <justin.li0423@gmail.com>
2018-05-25 19:41:20 -0400

    made a button :D

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import Colors from './data/Colors';
+
+export class Login extends Component {
+  render() {
+    return (
+      <Button>Sign in</Button>
+    );
+  }
+};
+
+const Button = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: ${Colors.green};
+    padding: 1em 2em;
+    font-size: 2.5em;
+    color: ${Colors.white};
+    border-radius: 35px;    
+`;
---
+++ src/components/data/Colors.js
@@ -0,0 +1,6 @@
+const colors = {
+    green: '#1db954',
+    white: '#ffffff',
+}
+
+export default colors;
